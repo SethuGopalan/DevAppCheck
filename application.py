d0e09ea4edc6ade046be3c418253e46af506e382
Sethu Gopalan <sethugopalan@outlook.com>
2023-06-20 23:48:48 -0400

    app aded

---
+++ application.py
@@ -0,0 +1,32 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc,html,Dash
+
+
+
+application=Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+server=application.server
+
+application.layout=dbc.Container(
+
+    [
+        dbc.Row(
+            [
+                dbc.Col([
+
+                    html.Div([
+
+                        html.H1("DevWebApplication"),
+                        html.P("Welcome to webapplication page"),
+                        html.H2(" best web app"),
+                        html.H3("page2")
+                    ])
+                ])
+            ]
+        )
+    ]
+)
+
+if __name__ == "__main__":
+    application.run(debug=True,port=8000)
